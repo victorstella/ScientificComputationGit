8c257c505210fe7ff00b01aa1e799863bd1c75f7
victorstella <victor.stella96@gmail.com>
2021-09-30 19:32:12 -0300

    getting started

--- EP-01/index.c
+++ EP-01/index.c
@@ -4,24 +4,34 @@
 #include <matheval.h>
 
 void main(int argc, char **argv) {
-  char *fx, *diffFx;
+  char *fx;
   double x0, epsilon;
   int max_iter;
 
   char *endptr;
+  void *diffFx;
 
   // Verify correct number of inputs
-  if (argc != 4) {
-    perror("Incorrect number of inputs, 4 expected!");
-    exit(1);
-  }
+  //if (argc != 4) {
+  //  perror("Incorrect number of inputs, 4 expected!");
+  //  exit(1);
+  //}
 
   // Fill the variables with the respective input information
   fx = (char *) calloc(strlen(argv[0]), sizeof(char));
   strcpy(fx, argv[0]);
-  x0 = strtod(argv[1], &endptr);
-  epsilon = strtod(argv[2], &endptr);
-  max_iter = atoi(argv[3]);
+  //x0 = strtod(argv[1], &endptr);
+  //epsilon = strtod(argv[2], &endptr);
+  //max_iter = atoi(argv[3]);
 
-  evaluator_derivative_x();
+  void *Fx = evaluator_create(fx);
+  diffFx = evaluator_derivative_x(Fx);
+
+  printf("f(4) = %s\n", evaluator_get_string(diffFx));
+
+  evaluator_destroy (Fx);
+  evaluator_destroy (diffFx);
+  free(fx);
+
+  exit(1);
 }
