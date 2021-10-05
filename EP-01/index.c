2f18d68c4a0b131e769d9f8068bc55a0f49e9483
victor.stella96 <victor.stella96@gmail.com>
2021-10-05 19:45:56 -0300

    1st part and docsExample1

--- EP-01/index.c
+++ EP-01/index.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <float.h>
+
 #include <matheval.h>
+
 #include <assert.h> // Para função assert (debugagem)
 
-void main(int argc, char **argv) {
+#define ZERO 0
+
+int main(int argc, char **argv) {
   void *f; // Função de entrada
   char *fx = NULL;
-  double x0, epsilon;
-  int max_iter;
+  double x0 = 1, epsilon = DBL_EPSILON;
+  int max_iter = 1;
 
-  char *endptr;
-  void *diffFx;
+  //char *endptr;
+  void *diffFx = NULL;
 
   size_t len = 0;
   ssize_t lnSize;
   int lnTam;
+
+  double fxTrueZero = 0;
+  void *aux = NULL;
+
 
   // Verify correct number of inputs
   //if (argc != 4) {
@@ -34,7 +43,7 @@ void main(int argc, char **argv) {
   printf(">> %s, (%d)", fx, lnTam);
   f = evaluator_create(fx);
   assert(f);
-
+  
   if(f == NULL){
     perror("Erro na expressão");
     exit(0);
@@ -43,17 +52,30 @@ void main(int argc, char **argv) {
     //exit(1);
   }
 
+  fxTrueZero = evaluator_evaluate_x(f, ZERO);
   diffFx = evaluator_derivative_x(f);
+  x0 = evaluator_evaluate_x(diffFx, ZERO);
+
+  for(max_iter; abs(x0 - fxTrueZero) <= epsilon; max_iter++) {
+    aux = diffFx;
+    diffFx = evaluator_derivative_x(aux);
+    x0 = evaluator_evaluate_x(diffFx, ZERO);
+  }
+
+
+
 
   printf("f'(x) = %s\n", evaluator_get_string(diffFx));
+  printf("f'(0) = %1.16e\n", x0);
 
+
+  printf("ate aqui ok");
   evaluator_destroy (f);
   evaluator_destroy (diffFx);
 
   free(f);
   free(diffFx);
   free(fx);
-  free(endptr);
 
   exit(1);
 }
