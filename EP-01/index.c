abdf46f07a4cecdfc0a2470761bac37b9811fe9e
victor.stella96 <victor.stella96@gmail.com>
2021-10-05 23:42:20 -0300

    input and loop

--- EP-01/index.c
+++ EP-01/index.c
@@ -12,36 +12,41 @@
 int main(int argc, char **argv) {
   void *f, *diffFx; // Função de entrada e derivada
   char *fx;
-  double x0;
+  double x0, epsilon;
   int max_iter = 0;
   
   size_t len = 0;
-  ssize_t lnSize;
-  int lnTam;
 
   double fxTrueZero = 0;
 
-  lnTam = getline(&fx, &len, stdin);
-  printf(">> f(x) = %s, (%d)\n", fx, lnTam);
+  getline(&fx, &len, stdin);
+  fx[strcspn (fx, "\n")] = '\0';
+  scanf("%lf", &x0);
+  scanf("%lf", &epsilon);
+  scanf("%d", &max_iter);
+
+  printf("%s\n", fx);
+  printf("%le\n", x0);
+  printf("%le\n", epsilon);
+  printf("%d\n", max_iter);
   f = evaluator_create(fx);
   assert(f);
   
   if(f == NULL){
-    perror("Erro na expressão");
+    perror("Erro na entrada de dados.");
     exit(0);
   }
 
   fxTrueZero = evaluator_evaluate_x(f, ZERO);
   diffFx = evaluator_derivative_x(f);
-  printf(">> f'(x) = %s\n", evaluator_get_string(diffFx));
+  printf("\n>> f'(x) = %s\n\n", evaluator_get_string(diffFx));
 
-  x0 = evaluator_evaluate_x(diffFx, ZERO);
-  printf("%d, %1.16e, %1.16e\n", max_iter, x0, fabs(x0 - fxTrueZero));
+  printf("0, %1.16e, %1.16e\n", x0, x0);
 
-  for(max_iter = 1; fabs(x0 - fxTrueZero) >= __DBL_EPSILON__; max_iter++) {
+  for(int i = 1; fabs(x0 - fxTrueZero) >= epsilon && i <= max_iter; i++) {
     diffFx = evaluator_derivative_x(diffFx);
     x0 = evaluator_evaluate_x(diffFx, ZERO);
-    printf("%d, %1.16e, %1.16e\n", max_iter, x0, fabs(x0 - fxTrueZero));
+    printf("%d, %1.16e, %1.16e\n", i, x0, fabs(x0 - fxTrueZero));
   }
 
   evaluator_destroy(f);
