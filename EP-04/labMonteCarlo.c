f1daae98e7de1c2a123f05316b89c9a1981b12c4
edurossob <edurossob@gmail.com>
2021-12-06 18:05:24 -0300

    Melhora Tang

--- EP-04/labMonteCarlo.c
+++ EP-04/labMonteCarlo.c
@@ -3,10 +3,9 @@
 #include <math.h>
 
 #include "utils.h"
-// #include "randomNR.h"
 
 #define DIFF 0.0
-// #define POINTS_NUMBER 10000000
+#define POINTS_NUMBER 10000000
 
 #define NRAND    ((double) rand() / RAND_MAX)  // drand48() 
 #define SRAND(a) srand(a) // srand48(a)
@@ -19,24 +18,22 @@ double styblinskiTang(double a, double b, int namostras) {
   printf("Metodo de Monte Carlo (x, y).\n");
   printf("a = (%f), b = (%f), n = (%d), variaveis = 2\n", a, b, namostras);
 
-  double interval = b - a, randX = 0, soma_aux_1 = 0, soma_aux_2 = 0, soma_total = 0;
+  double interval = b - a, randX = 0, soma_aux_1 = 0, soma_aux_2 = 0, soma_total = 0, x_square = 0;
 
   double t_inicial = timestamp();
 
   for (int i = 0; i < namostras; i++) {
     randX = a + NRAND * interval;
-    soma_aux_1 = fabs(pow(randX, 4) - (16 * pow(randX, 2)) + (5 * randX));
+    x_square = randX * randX;
+    soma_aux_1 = (x_square * x_square) - (16 * x_square) + (5 * randX);
+    
     randX = a + NRAND * interval;
-    soma_aux_2 = fabs(pow(randX, 4) - (16 * pow(randX, 2)) + (5 * randX));
+    x_square = randX * randX;
+    soma_aux_2 = (x_square * x_square) - (16 * x_square) + (5 * randX);
 
-    soma_total = soma + ((soma_aux_1 + soma_aux_2) / 2);    
-    soma = (soma_aux_1 + soma_aux_2) / 2;
+    soma_total = soma + ((soma_aux_1 + soma_aux_2) / 2);
+    soma = soma_total;
   }
-
-  /* for (int j = 0; j < namostras; j++) {
-    randX = a + NRAND * interval;
-    soma_j += (pow(randX, 4) - (16 * pow(randX, 2)) + (5 * randX)) / 2;
-  } */
 
   resultado = interval * (soma_total / namostras);
   
@@ -59,7 +56,7 @@ double retangulos_xy(double a, double b, int npontos) {
   printf("a = (%f), b = (%f), n = (%d), h = (%f)\n", a, b, npontos, h);
   
   double dx_i = a, dx_j = a;
-  double soma_aux_1 = 0, soma_aux_2 = 0, soma_total_1 = 0, soma_total_2 = 0, dxAux = 0;
+  double soma_aux = 0, soma_total_1 = 0, soma_total_2 = 0, dxAux = 0, dx_square = 0;
 
   double t_inicial = timestamp();
   
@@ -67,23 +64,25 @@ double retangulos_xy(double a, double b, int npontos) {
 
   printf("começou1");
   for (int i = 0; i < npontos; i++) {
-    soma_aux_1 = fabs(pow(dx_i, 4) - (16 * pow(dx_i, 2)) + (5 * dx_i));
-    soma_aux_2 = fabs(pow(dx_i, 4) - (16 * pow(dx_i, 2)) + (5 * dx_i));
-    soma_total_1 = soma_i + ((soma_aux_1 + soma_aux_2) / 2);
-    soma_i = (soma_aux_1 + soma_aux_2) / 2;
+    dx_square = dx_i * dx_i;
+    
+    soma_aux = (dx_square * dx_square) - (16 * dx_square) + (5 * dx_i);
+    
+    soma_total_1 = soma_i + soma_aux;
+    soma_i = soma_total_1;
 
     dxAux = dx_i;
     dx_i = dxAux + h;
   }
 
-  soma_aux_1 = 0;
-  soma_aux_2 = 0;
   printf("começou2");
   for (int j = 0; j < npontos; j++) {
-    soma_aux_1 = fabs(pow(dx_j, 4) - (16 * pow(dx_j, 2)) + (5 * dx_j));
-    soma_aux_2 = fabs(pow(dx_j, 4) - (16 * pow(dx_j, 2)) + (5 * dx_j));
-    soma_total_2 = soma_j + ((soma_aux_1 + soma_aux_2) / 2);
-    soma_j = (soma_aux_1 + soma_aux_2) / 2;
+    dx_square = dx_j * dx_j;
+
+    soma_aux = (dx_square * dx_square) - (16 * dx_square) + (5 * dx_j);
+
+    soma_total_2 = soma_j + soma_aux;
+    soma_j = soma_total_2;
 
     dxAux = dx_j;
     dx_j = dxAux + h;
@@ -92,7 +91,7 @@ double retangulos_xy(double a, double b, int npontos) {
   // xi ok
   // h ok
   
-  resultado = soma_total_1 * soma_total_2 * pow(h, 2);
+  resultado = soma_total_1 * soma_total_2 * h * h;
 
   printf("\n\n***soma_i: %1.16f, soma_j: %1.16f resultado: %1.16f, dx_i: %1.16f, dx_j: %1.16f\n\n", soma_i, soma_j, resultado, dx_i, dx_j);
   
@@ -106,18 +105,22 @@ double retangulos_xy(double a, double b, int npontos) {
 
 int main(int argc, char **argv) {
 
-  if (argc < 5) {
+  if (argc < 3) {
     printf("Utilização: %s inicial final n_amostras n_variaveis\n", argv[0]);
     return 1;
   }
 
   double a = atof(argv[1]), b = atof(argv[2]);
-  int nAmostras = atoi(argv[3]), nVars = atoi(argv[4]);
+  
+  if (atoi(argv[3]) < 2 || atoi(argv[3]) == 3 || (atoi(argv[3]) > 4 && atoi(argv[3]) < 8) || atoi(argv[3]) > 8) {
+    perror("Número de dimensões inválido.\n");
+    exit(1);
+  }
 
   SRAND(20211);
 
-  double monteCarlo = styblinskiTang(a, b, nAmostras);
-  double retangulos = retangulos_xy(a, b, nAmostras);
+  double monteCarlo = styblinskiTang(a, b, POINTS_NUMBER);
+  double retangulos = retangulos_xy(a, b, POINTS_NUMBER);
 
   printf("Resultado pelo método de Monte Carlo: %1.16f\n", monteCarlo);
   printf("Resultado pelo método dos Retângulos: %1.16f\n", retangulos);
