fdb2841172e64cffd8c3991b986fab12fc5620c1
Victor Stella <victor.stella96@gmail.com>
2021-12-14 13:48:20 -0300

    sl_t

--- T-2/erb19-vdsp20/sl_t.c
+++ T-2/erb19-vdsp20/sl_t.c
@@ -2,16 +2,17 @@
 
 // ASSUMINDO QUE É TRIDIAGONAL
 
-function_t* inicia_function_t(char* s_func, double solution, int var_count) {
-    function_t* func = malloc(sizeof(function_t));
+function_t* inicia_function_t(char* s_func, double solution, int * var_count) {
+    function_t* func = calloc(1, sizeof(function_t));
     
     func->s_func = strdup(s_func);
     func->v_func = evaluator_create(s_func);
+
+    evaluator_get_variables(func, func->var_names, var_count); 
     
     for(int i = 0; i < var_count; i++)
         func->x_aprox[i] = 0;
 
-    evaluator_get_variables(func, func->var_names, var_count); 
 
     return func;
 }
@@ -23,15 +24,17 @@ function_t* inicia_function_t(char* s_func, double solution, int var_count) {
  * @return sistema linear sl_t
  */ 
 sl_t * inicia_sl_t(int n, char ** funcs, double* x_aprox) {
-    sl_t * sl = malloc(sizeof(sl_t));
+    sl_t * sl = calloc(1, sizeof(sl_t));
+    int * var_count = calloc(n, sizeof(int));
 
     sl->n = n;
     sl->func = calloc(n, sizeof(function_t));
     
     for (int i = 0; i < n; i++) {
-        sl->func[i].s_func = strdup(funcs[i]);
+        inicia_function_t(sl->func[i].s_func, x_aprox[i], var_count);
+        /* sl->func[i].s_func = strdup(funcs[i]);
         sl->func[i].v_func = evaluator_create(funcs[i]);
-        sl->func[i].x_aprox = x_aprox;
+        sl->func[i].x_aprox = x_aprox; */
     }    
 
     return sl;
